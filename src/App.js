ba24506fd82f35555f0324e32aa933e661d7adf0
Tomasz Chmiel <tomasz.chmiel@pagepro.pl>
2018-04-28 10:46:18 +0200

    step-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import Home from './Home'
+import Setup from './Setup'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      nick: '',
+      difficulty: null
+    }
+
+    this.setupApp = this.setupApp.bind(this)
+  }
+
+  setupApp (nick, difficulty) {
+    this.setState({
+      nick,
+      difficulty
+    })
+  }
+
   render () {
     return (
       <span>
         <Home
-          name='Name'
+          name={this.state.nick}
+        />
+        <Setup
+          setupApp={this.setupApp}
         />
       </span>
     )
---
+++ src/Setup.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Setup extends Component {
+  constructor (props) {
+    super(props)
+
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = {
+      name: '',
+      difficulty: ''
+    }
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+
+    const {
+      props: {
+        setupApp
+      },
+      refs: {
+        nickName: {
+          value: nickNameVal
+        },
+        difficulty: {
+          value: difficultyVal
+        }
+      }
+    } = this
+
+    if (!nickNameVal.length) {
+      window.alert('Please enter the name :)')
+      return
+    }
+
+    setupApp(nickNameVal, difficultyVal)
+  }
+
+  render () {
+    return (
+      <div className='l-centered'>
+        <form
+          className='f-start'
+          onSubmit={this.onSubmit}
+        >
+          <label
+            className='f-start__label'
+            htmlFor='nickname'
+          >
+            Nick:
+          </label>
+          <input
+            className='f-start__control'
+            type='text'
+            ref='nickName'
+            id='nickname'
+            autoFocus
+          />
+          <label
+            className='f-start__label'
+            htmlFor='difficulty'
+          >
+            Difficulty:
+          </label>
+          <select
+            ref='difficulty'
+            id='difficulty'
+            className='f-start__control'
+          >
+            <option
+              value='easy'
+            >
+              Easy
+            </option>
+            <option
+              value='medium'
+            >
+              Medium
+            </option>
+            <option
+              value='hard'
+            >
+              Hard
+            </option>
+          </select>
+          <button
+            className='f-start__action'
+            type='submit'
+          >
+            Start
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+Setup.propTypes = {
+  setupApp: PropTypes.func
+}
+
+export default Setup
