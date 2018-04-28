81aa62c763c3fc96ac14d1f2f5fb0e038d404173
Tomasz Chmiel <tomasz.chmiel@pagepro.pl>
2018-04-28 09:29:12 +0200

    init / step-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <span>
+        Hello world!
+      </span>
+    )
+  }
+}
+
+export default App
